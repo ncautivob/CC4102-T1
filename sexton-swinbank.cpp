a1e16f78fd5f423354f6b2f9d8273090e30c846f
Máximo Flores Valenzuela <maximo.flores@ug.uchile.cl>
2024-05-03 23:21:23 -0400

    updates algorithm

--- sexton-swinbank.cpp
+++ sexton-swinbank.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 typedef long double ld;
 
-#define B 4096
-
 using Point = pair<ld, ld>;
 
 typedef struct {
@@ -12,22 +10,48 @@ typedef struct {
   set<Entry>* children;  // If not set<Entry>, then it is a single point.
 } Entry;
 
-using Node = set<Entry>;  // Conjunto de puntos
-using Nodes = set<Node>;  // set<set<Entry>>
+#define ENTRY_SIZE sizeof(Entry)
+#define B 4096 / ENTRY_SIZE
+#define b 0.5 * B / ENTRY_SIZE
+
+using Node = set<Entry>;            // Entries set.
+using Nodes = set<Node>;            // Set that have entries set as elements.
+using Points = set<Point>;          // Set of points.
+using PointClusters = set<Points>;  // Set that have points set as elements.
 
 /**
- * Computes the euclidean distance between firstPoint and secondPoint.
+ * Computes the squared euclidean distance between firstPoint and secondPoint.
+ * As the square function strictly increases in the positive X-axis, then the
+ * result doesn't change.
  */
 ld distance(Point& firstPoint, Point& secondPoint) {
   ld delta_x = firstPoint.first - secondPoint.first;
   ld delta_y = firstPoint.second - secondPoint.second;
 
-  return (ld)sqrt(delta_x * delta_x + delta_y * delta_y);
+  return (ld)delta_x * delta_x + delta_y * delta_y;
 }
 
-Nodes nearestNodes(Nodes input) {
+/**
+ * Uses the MinMax split policy to separate a cluster into two.
+ */
+PointClusters splittingPolicy(Points input) {
+  // TODO
+}
+
+/**
+ * Joins two points set first and second.
+ */
+Points pointsUnion(Points* first, Points* second) {
+  Points result;
+  result.insert(first.begin(), first.end());
+  result.insert(second.begin(), second.end());
+
+  return result;
+}
+
+PointClusters nearestPointClusters(PointClusters input) {
   ld minDistance = 1e18;
-  Nodes candidateNearest = {NULL, NULL};
+  PointClusters candidateNearest = {NULL, NULL};
 
   /**
    * It is known that euclidean distance is symmetric, id. est., d(x, y) = d(y,
@@ -36,21 +60,20 @@ Nodes nearestNodes(Nodes input) {
    * way better that use the naive approach to do n^2 comparisons.
    */
   for (auto entry = input.begin(); entry != input.end(); entry++) {
-    Node node = *entry;
-    Entry medioid = computeMedioid(node);
-    Point point = medioid.point;
+    Points points = *entry;
+    Point medioid = computeMedioid(points);
+
     for (auto otherEntry = next(entry); otherEntry != input.end();
          otherEntry++) {
-      Node otherNode = *otherEntry;
-      Entry otherMedioid = computeMedioid(otherNode);
-      Point otherPoint = otherMedioid.point;
+      Points otherPoints = *otherEntry;
+      Point otherMedioid = computeMedioid(otherPoints);
 
-      // As we are using <=, we return the very last pair of clusters, if there
-      // are more than one.
-      ld dist = distance(point, otherPoint);
+      // As we are using <=, we return the very last pair of point clusters, if
+      // there are more than one that satisfies the inequality.
+      ld dist = distance(medioid, otherMedioid);
       if (dist <= minDistance) {
         minDistance = dist;
-        candidateNearest = {node, otherNode};
+        candidateNearest = {points, otherPoints};
       }
     }
   }
@@ -58,19 +81,17 @@ Nodes nearestNodes(Nodes input) {
   return candidateNearest;
 };
 
-Node nearestNeighbour(Node objective, Nodes nodes) {
-  Entry objectiveMedioid = computeMedioid(objective);
-  Point objectivePoint = objectiveMedioid.point;
+Points nearestNeighbour(Points objective, PointClusters clusters) {
+  Point objectiveMedioid = computeMedioid(objective);
 
-  Node candidateNearest = {};
+  Points candidateNearest = {};
   ld minDistance = 1e18;
 
-  for (auto entry = nodes.begin(); entry != nodes.end(); entry++) {
-    Node node = *entry;
-    Entry nodeMedioid = computeMedioid(node);
-    Point nodePoint = nodeMedioid.point;
+  for (auto entry = clusters.begin(); entry != clusters.end(); entry++) {
+    Points points = *entry;
+    Point pointsMedioid = computeMedioid(points);
 
-    ld dist = distance(objectivePoint, nodePoint);
+    ld dist = distance(objectiveMedioid, pointsMedioid);
     /**
      * <= is a relajation to choose the very last node that
      * verifies this condition. Dist must not be 0, in other
@@ -78,7 +99,7 @@ Node nearestNeighbour(Node objective, Nodes nodes) {
      */
     if (dist <= minDistance && dist != 0) {
       minDistance = dist;
-      candidateNearest = node;
+      candidateNearest = points;
     }
   }
 
@@ -86,14 +107,14 @@ Node nearestNeighbour(Node objective, Nodes nodes) {
 }
 
 /**
- * Computes a medioid given a set of points (cluster).
+ * Computes a medioid given a set of points.
  * If multiple medioids, it computes the last in the set order.
  */
-Entry computeMedioid(Node input) {
-  if (input.empty()) return {{0.0, 0.0}, NULL, NULL};
+Point computeMedioid(Points input) {
+  if (input.empty()) return {0.0, 0.0};
 
-  Entry initial = *(input.begin());
-  Entry candidateMedioid = initial;
+  Point initial = *(input.begin());
+  Point candidateMedioid = initial;
 
   // A very large number, because we want to minimize.
   ld minTotalDistance = (ld)1e18;
@@ -101,14 +122,12 @@ Entry computeMedioid(Node input) {
   for (auto entry = input.begin(); entry != input.end(); entry++) {
     // Initialize the total distance for this point.
     ld totalDistance = 0.0;
-    // The (p, c_r, a) tuple.
-    Entry tuple = *entry;
-    Point point = tuple.point;
+    // The current point to be compared.
+    Point point = *entry;
 
     for (auto otherEntry = input.begin(); otherEntry != input.end();
          otherEntry++) {
-      Entry otherTuple = *otherEntry;
-      Point otherPoint = otherTuple.point;
+      Point otherPoint = *otherEntry;
 
       totalDistance += distance(point, otherPoint);
     }
@@ -119,21 +138,23 @@ Entry computeMedioid(Node input) {
      */
     if (totalDistance <= minTotalDistance) {
       minTotalDistance = totalDistance;
-      candidateMedioid = tuple;
+      candidateMedioid = point;
     }
   }
 
   return candidateMedioid;
 }
 
-Entry outputHoja(Node input) {
-  Entry primaryMedioid = computeMedioid(input);
-  Point g = primaryMedioid.point;
+/**
+ * Returns a tuple (g, r, a) where g is the primary medioid of the input,
+ * r is the covering radius, and a the node direction.
+ */
+Entry OutputLeafPage(Points input) {
+  Point g = computeMedioid(input);
   ld r = 0.0;
   Node cluster;
 
-  for (Entry entry : input) {
-    Point p = entry.point;
+  for (Point p : input) {
     Entry newEntry = {p, NULL, NULL};
 
     cluster.insert(newEntry);
@@ -144,15 +165,24 @@ Entry outputHoja(Node input) {
   return {g, r, a};
 }
 
-Entry outputInterno(Node input) {
-  Entry primaryMedioid = computeMedioid(input);
-  Point G = primaryMedioid.point;
+/**
+ * Returns a tuple (G, R, A) where G is the primary medioid of the points
+ * that belongs to the input node, R is the covering radius and A the node
+ * direction.
+ */
+Entry OutputInternalPage(Node input) {
+  Points C_in;
+  // We add the points in the input node to C_in.
+  for (Entry entry : input) C_in.insert(entry.point);
+
+  Point G = computeMedioid(C_in);
   ld R = 0.0;
   Node cluster;
 
   for (Entry entry : input) {
     Point g = entry.point;
     ld r = entry.coveringRadius;
+    Entry* a = entry.children;
 
     cluster.insert(entry);
     R = max(R, distance(G, g) + r);
@@ -162,33 +192,78 @@ Entry outputInterno(Node input) {
   return {G, R, A};
 }
 
-Entry SSAlgorithm(Node input) {
-  if (sizeof(input) <= B) {
-    Entry leaf = outputHoja(input);
-    return leaf;
+Entry* SSAlgorithm(Points input) {
+  // There are at most B points.
+  if (input.size() <= B) {
+    Entry rootRepresentation = OutputLeafPage(input);
+    return rootRepresentation.children;
   }
 
-  Nodes out = cluster(input);
-  Node C;
+  // We have > B points. Then, we must do clustering to reduce size.
+  Nodes C_out = cluster(input);
+  Node C = {};
 
-  for (Node Node : out) C.insert(outputHoja(Node));
-
-  // Falta del paso 4 en abajo
-  while (sizeof(C) > B) {
+  for (Points points : C_out) {
+    C.insert(OutputLeafPage(points));
   }
+
+  /**
+   * We do this in order to retrieve an entry given a point, to optimize
+   * search. Warning: Saving is global, and it doesn't reset in every while
+   * iteration, this may be a problem.
+   */
+  unordered_map<Point, Entry> pointToEntry;
+
+  while (C.size() > B) {
+    Points C_in;
+
+    // We insert into C_in the points contained in C.
+    for (Entry entry : C) {
+      Point point = entry.point;
+      C_in.insert(point);
+
+      // Only add to the mapping if is not present.
+      if (pointToEntry.find(point) == pointToEntry.end()) {
+        pointToEntry[point] = entry;
+      }
+    }
+
+    // Clustering again to reduce set size of C.
+    C_out = cluster(C_in);
+    Node C_mra = {};
+
+    for (Points points : C_out) {
+      Node s;
+      for (Point point : points) {
+        s.insert(pointToEntry[point]);
+      }
+
+      C_mra.insert(s);
+    }
+
+    C = {};
+    for (Node node : C_mra) {
+      C.insert(OutputInternalPage(node));
+    }
+  }
+
+  Entry rootRepresentation = OutputInternalPage(C);
+  return rootRepresentation.children;
 }
 
-Nodes cluster(Node input) {
-  Nodes cluster_output, aux_cluster;
+/**
+ * Split a set of points into clusters of size at least b and size at most B.
+ */
+PointClusters cluster(Points input) {
+  PointClusters C_out = {}, C = {};
 
-  for (Entry p : input) {
-    Node newNode;
-    newNode.insert(p);  // Insertamos el singleton {p}
-    aux_cluster.insert(newNode);
+  for (Point p : input) {
+    // We insert the singleton {p}.
+    C.insert(p);
   }
 
-  while (aux_cluster.size() > 1) {
-    Nodes nearest = nearestNodes(aux_cluster);
+  while (C.size() > 1) {
+    PointClusters nearest = nearestPointClusters(C);
 
     // Nearest must only be two, as nearestNodes returns two nodes.
     // We retrieve this data to erase the iterators from the set.
@@ -200,35 +275,46 @@ Nodes cluster(Node input) {
       swap(first, second);
     }
 
-    Node firstNode = *first;
-    Node secondNode = *second;
+    Points firstPointSet = *first;
+    Points secondPointSet = *second;
 
-    Node unionNode;
-    unionNode.insert(firstNode.begin(), firstNode.end());
-    unionNode.insert(secondNode.begin(), secondNode.end());
+    Points firstUnion = pointsUnion(firstPointSet, secondPointSet);
 
-    if (sizeof(unionNode) <= B) {
-      aux_cluster.erase(first);
-      aux_cluster.erase(second);
+    if (firstUnion.size() <= B) {
+      C.erase(first);
+      C.erase(second);
 
-      aux_cluster.insert(unionNode);
+      C.insert(firstUnion);
     } else {
-      aux_cluster.erase(first);
-      cluster_output.insert(firstNode);
+      C.erase(first);
+      C_out.insert(firstPointSet);
     }
   }
 
-  Node lastNode = *(prev(aux_cluster.end()));
-  Node nearestToLast = {};
-  if (sizeof(cluster_output) > 0) {
-    nearestToLast = nearestNeighbour(lastNode, cluster_output);
-    cluster_output.erase(cluster_output.find(nearestToLast));
+  Points lastPointSet = *(prev(C.end()));
+  Points nearestToLast = {};
+  if (C_out.size() > 0) {
+    nearestToLast = nearestNeighbour(lastPointSet, C_out);
+    C_out.erase(C_out.find(nearestToLast));
   }
 
-  // Falta del paso 6 en adelante
+  Points secondUnion = pointsUnion(lastPointSet, nearestToLast);
+  if (secondUnion.size() <= B) {
+    C_out.insert(secondUnion);
+  } else {
+    PointClusters separated = splittingPolicy(secondUnion);  // TODO
+
+    Points newFirst = *(separated.first());
+    Points newSecond = *(prev(separated.last()));
+
+    C_out.insert(newFirst);
+    C_out.insert(newSecond);
+  }
+
+  return C_out;
 };
 
 int main() {
-  int a = 1;
+  // The test set goes here...
   return 0;
 }
